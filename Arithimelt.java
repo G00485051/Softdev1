868ec58ea0c9d5d5b3537d25af40de07e3e59748
G00485051 <G00485051@atu.ie>
2026-01-21 14:37:56 +0000

    Add files via upload

---
+++ Arithimelt.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Arithimelt{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int first , second, third, result;
+        System.out.print("Enter first namber: ");
+        first = input.nextInt();
+        System.out.print("Enter second namber: ");
+        socond = input.nextInt();
+
+
+        result = first + second;
+        System.out.println(first + " + " + second + " = " + result);
+
+        third = 10;
+
+        System.out.print(result);
+
+        result += third;
+
+        System.out.println(" + " + third + " = " + result);
+        input.close();
+
+    }
+}
